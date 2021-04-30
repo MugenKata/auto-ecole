8d6b1d23ccc744a1707960d0cfc597e49398844d
Adrien Delacroix <ad.dela>
2021-04-30 19:01:08 +0200

    c'est nul

--- vue/accueil.php
+++ vue/accueil.php
@@ -1,8 +1,8 @@
 <section class="py-5 text-center container bg">
     <div class="row py-lg-5">
       <div class="col-lg-6 col-md-8 mx-auto">
-        <h1 class="fw-light">La Societe AutoHiro</h1>
-        <p class="lead text-muted">L'auto-école AutoHiro à Paris et à Plougerscant en Bretagne propose ses formations Auto, Moto et AM (BSR) avec un seul but : la réussite et la sécurité de ses élèves.
+        <h1 class="fw-light">La Societe HiroAuto</h1>
+        <p class="lead text-muted">L'auto-école HiroAuto à Paris et à Plougerscant en Bretagne propose ses formations Auto, Moto et AM (BSR) avec un seul but : la réussite et la sécurité de ses élèves.
 
 		Chaque formation est dispensée par un enseignant diplômé, expérimenté et sérieux.
 
